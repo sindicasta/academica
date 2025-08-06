878beef01894a80697c1ee5bf27aa03a76724763
sindicasta <scastanedam1@miumg.edu.gt>
2025-08-05 21:16:17 -0600

    tarea 4

---
+++ src/main/java/com/academica/academica/AcademicaApplication.java
@@ -0,0 +1,13 @@
+package com.academica.academica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academica/academica/AcademicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academica.academica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
